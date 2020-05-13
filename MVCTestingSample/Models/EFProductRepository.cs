8e7c3305cd7d4644b7c430d44c91581cc9f5ab09
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-13 08:59:16 -0700

    Created productRespository and default implementation

---
+++ MVCTestingSample/Models/EFProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using MVCTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models
+{
+    /// <summary>
+    /// Implements interface, lightbulb
+    /// </summary>
+    public class EFProductRepository : IProductRespository
+    {
+
+        public Task AddProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
