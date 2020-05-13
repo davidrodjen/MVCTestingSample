e92934edc992057eff950d1f23d66287e19bd901
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-13 08:58:45 -0700

    Create DB Context

---
+++ MVCTestingSample/Models/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models
+{
+    /// <summary>
+    /// ProductDbContext inherits DbContext, install microsoft.entityframeworkcore
+    /// </summary>
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
