b22d6854d83e3737ac6b0abc31d92fdcabe58fe5
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-13 08:10:14 -0700

    Add project files.

---
+++ MVCTestingSampleTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCTestingSample.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTestingSample.Controllers.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
